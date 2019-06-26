bcba6a6242756daff4fc7d6ae02f32028a705111
AJ Guiles <ashiguiles@gmail.com>
2019-06-26 01:20:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,17 +6,17 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/' do
-    text_from_user = params[:user_text]
+  # post '/' do
+  #   text_from_user = params[:user_text]
     
-    # pass data from param[user_text] into a new instance of theTextAnalyzer class
+  #   # pass data from param[user_text] into a new instance of theTextAnalyzer class
     
-    # @analyzed_text = TextAnalyzer.new(text_from_user)
-     #the above can shortened to the following
-      @analyzed_text = TextAnalyzer.new(params[:user_text])
+  #   # @analyzed_text = TextAnalyzer.new(text_from_user)
+  #   #the above can shortened to the following
+  #     @analyzed_text = TextAnalyzer.new(params[:user_text])
  
-    erb :results
-  end
+  #   erb :results
+  # end
   
   # We now have the instance of TextAnalyzer saved to an instance variable called @analyzed_text. This can now be called from the results.erb view being rendered using web tags. 
 end
